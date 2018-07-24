f573b92aaef7d5668552a3967fdc129cd009b1ee
Andrej Perfilov [home] <33530333+p3rfilov@users.noreply.github.com>
2018-07-24 21:01:48 +0100

    added specular to phong shading

--- First Steps (with ModernGL)/07_Basic_Lighting.py
+++ First Steps (with ModernGL)/07_Basic_Lighting.py
@@ -1,11 +1,11 @@
 # working with colours and applying basic lighting
-# use W, S, A, D, SPACE & LCTRL keys to move the light
+# use W, S, A, D, SPACE & LCTRL keys or the mouse (click and drag) to move the light
 
 import moderngl
 import numpy as np
 import pyglet as pg
 from pyglet.gl import *
-from pyglet.window import key
+from pyglet.window import key, mouse
 import pyrr
 from pyrr import Matrix44, Vector3
 
@@ -40,21 +40,28 @@ fs_box = '''
     uniform vec3 objectColour;
     uniform vec3 lightColour;
     uniform vec3 lightPos;
+    uniform vec3 viewPos;
     in vec3 fragPos;
     in vec3 fragNorm;
     out vec4 fragColour;
     
     void main()
     {
+        float ambientStrength = 0.1;
+        vec3 ambient = ambientStrength * lightColour;
+        
         vec3 norm = normalize(fragNorm);
         vec3 lightDir = normalize(lightPos - fragPos);
         float diff = max(dot(norm, lightDir), 0.0);
         vec3 diffuse = diff * lightColour;
         
-        float ambientStrength = 0.1;
-        vec3 ambient = ambientStrength * lightColour;
+        float specularStrength = 0.5;
+        vec3 viewDir = normalize(viewPos - fragPos);
+        vec3 reflectDir = reflect(-lightDir, norm);
+        float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
+        vec3 specular = specularStrength * spec * lightColour;
         
-        vec3 result = (ambient + diffuse) * objectColour;
+        vec3 result = (ambient + diffuse + specular) * objectColour;
         fragColour = vec4(result, 1.0);
     }
     '''
@@ -136,9 +143,9 @@ vertices = np.array([
 glEnable(GL_DEPTH_TEST)
 
 cameraSpeed = 0.2
-cameraPos = [1.0, 2.0, -3.0]
-cameraTarget = [0.5, 0.5, -1.0]
-cameraUp = [0.0, 1.0, 0.0]
+cameraPos = Vector3([1.0, 2.0, -3.0])
+cameraTarget = Vector3([0.5, 0.5, -1.0])
+cameraUp = Vector3([0.0, 1.0, 0.0])
 view = Matrix44.look_at(cameraPos, cameraTarget, cameraUp)
 proj = Matrix44.perspective_projection(45.0, width / height, 0.1, 1000.0)
 
@@ -150,17 +157,24 @@ lightCol = Vector3([1.0, 1.0, 1.0])
 def on_key_press(symbol, modifier):
     global lightPos
     if symbol == key.W:
-        lightPos -= cameraSpeed * lightPos
+        lightPos.z += cameraSpeed
     if symbol == key.S:
-        lightPos += cameraSpeed * lightPos
+        lightPos.z -= cameraSpeed
     if symbol == key.A:
-        lightPos += pyrr.vector.normalize(np.cross(lightPos, [0.0, 1.0, 0.0])) * cameraSpeed
+        lightPos.x += cameraSpeed
     if symbol == key.D:
-        lightPos -= pyrr.vector.normalize(np.cross(lightPos, [0.0, 1.0, 0.0])) * cameraSpeed
+        lightPos.x -= cameraSpeed
     if symbol == key.SPACE:
         lightPos.y += cameraSpeed
     if symbol == key.LCTRL:
         lightPos.y -= cameraSpeed
+        
+@window.event
+def on_mouse_drag(x, y, dx, dy, buttons, modifiers):
+    global lightPos
+    if buttons & mouse.LEFT:
+        lightPos.z += dy / 200
+        lightPos.x -= dx / 200
 
 def buildTransMatrix(pos=[0,0,0], rot=[0,0,0], scale=[1,1,1]):
     trans = Matrix44.from_translation(pos)
@@ -182,6 +196,7 @@ def drawBox():
     box_prog['objectColour'].write(objCol.astype('f4').tobytes())
     box_prog['lightColour'].write(lightCol.astype('f4').tobytes())
     box_prog['lightPos'].write(lightPos.astype('f4').tobytes())
+    box_prog['viewPos'].write(cameraPos.astype('f4').tobytes())
     vao.render()
     
 def drawLight():
@@ -192,7 +207,7 @@ def drawLight():
     # 6 data values per vertex
     # 4 * 6 = 24
     vao.bind(0, 'f', vbo, '4f', stride=24)
-    model = buildTransMatrix(pos=lightPos, scale=[0.2, 0.2, 0.2])
+    model = buildTransMatrix(pos=lightPos, scale=[0.1, 0.1, 0.1])
     light_prog['projection'].write(proj.astype('f4').tobytes())
     light_prog['view'].write(view.astype('f4').tobytes())
     light_prog['model'].write(model.astype('f4').tobytes())
